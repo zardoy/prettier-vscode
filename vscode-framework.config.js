a3b580e2e005ec60352d863b555aa05aae03740c
Vitaly Turovsky <vital2580@icloud.com>
2022-01-02 20:26:40 +0300

    skip bundling for web for now

--- vscode-framework.config.js
+++ vscode-framework.config.js
@@ -3,7 +3,7 @@
 /** @type{import('vscode-framework/build/config').UserConfig} */
 const config = {
   prependIds: false,
-  target: {web: true,desktop: true},
+  // target: {web: true,desktop: true},
   // maybe future refactorings will change this
   consoleStatements: false
 };
